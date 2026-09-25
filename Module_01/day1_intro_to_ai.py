4dc64cda5471511ad29eace56c9e2d3946dd73fc
MaryMukai <mukaimary27@gmail.com>
2026-09-25 13:13:59 +0300

    Add introductory script for AI and Python module
    This script provides an introduction to AI, ML, and DL with a focus on their applications in East Africa's Fintech ecosystem.

---
+++ Module_01/day1_intro_to_ai.py
@@ -0,0 +1,19 @@
+"""
+Codomax Internship - Module 1 (Day 1)
+Introduction to AI & Python
+Context: Overview of AI applications in East Africa's Fintech Ecosystem
+"""
+
+print("==================================================")
+print("  CODOMAX INTERNSHIP - MODULE 1: AI & PYTHON    ")
+print("==================================================")
+print("Theme: Mobile Money Fraud Detection & Safety Guard\n")
+
+# Core Definitions
+ai_def = "Artificial Intelligence: AI is the ability of computers or machines to perform tasks that normally require human intelligence, such as making decisions, recognizing patterns or understanding information.."
+ml_def = "Machine Learning: a part of AI where computers learn from data and past examples. Instead of giving the computer every instruction, we allow it to find patterns and use them to make predictions or decisions.."
+dl_def = "Deep Learning: a more advanced form of Machine Learning. It uses neural networks to learn from large and complex amounts of data, such as text, images and transaction records.."
+
+print("1. " + ai_def)
+print("2. " + ml_def)
+print("3. " + dl_def)
